e593c23c2227caf6313573ebe03e5ee6a4552f7f
y <yyapwt@gmail.com>
2025-02-13 16:49:40 +0800

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,58 +1,20 @@
+#include "csv.hpp"
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
 
+using namespace csv;
 using namespace std;
 
-void readFile() {
-    string line;
-    ifstream trueNews("true.csv");
-    ifstream fakeNews("fake.csv");
-    vector<vector<string>> data;
-
-    if (!trueNews.is_open()) cout << "Error opening 'true.scv' !" << endl;
-    if (!fakeNews.is_open()) cout << "Error opening 'fake.scv' !" << endl;
-    
-    // Read the file line by line
-    while (getline(trueNews, line)) {
-        stringstream ss(line);
-        string cell;
-        vector<string> row;
-
-        // Split the line by commas
-        while (getline(ss, cell, '\t')) {
-            row.push_back(cell);
-        }
-
-        // Add the row to the data
-        data.push_back(row);
-    }
-    trueNews.close();
-
-    while (getline(fakeNews, line)) {
-        stringstream ss(line);
-        string cell;
-        vector<string> row;
-
-        while (getline(ss, cell, '\t')) {
-            row.push_back(cell);
-        }
-
-        data.push_back(row);
-    }
-    fakeNews.close();
-}
+CSVReader reader("fake.csv");
 
 int main() {
-/*
-    // Print the contents of the 2D vector
-    for (const auto& row : data) {
-        for (const auto& cell : row) {
-            cout << cell << " ";
+    int count = 0;
+
+    for (CSVRow &row : reader) {
+        for (CSVField &field : row) {
+            if ((field.get()) == " " || field.get().empty())
+                cout << row.to_json_array() << endl;
+
         }
-        cout << endl;
     }
-*/
-    return 0;
+    cout << "Count: " << count << endl;
 }
