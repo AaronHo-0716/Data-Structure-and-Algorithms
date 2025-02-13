976a9021f01fc783320366dff94a8c9edbdeb6bc
y <yyapwt@gmail.com>
2025-02-13 23:51:04 +0800

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -5,25 +5,44 @@
 using namespace csv;
 using namespace std;
 
-CSVReader reader("fake.csv");
-
-int main() {
-    string (*fake)[4] = new string[30000][4]; // head allcation of an array
+string (*readFake())[4] { 
+    CSVReader reader("fake.csv");
+    string (*fakeN)[4] = new string[23500][4]; // head allcation of an array
     int rowIdx = 0;
 
     for (CSVRow &row : reader) {
         int colIdx = 0;
 
         for (CSVField &field : row) {
-            fake[rowIdx][colIdx] = (field.get() == " " || (field.get()).empty()) ? "NULL" : field.get();
+            fakeN[rowIdx][colIdx] = (field.get() == " " || (field.get()).empty()) ? "NULL" : field.get();
             colIdx++;
         }
         rowIdx++;
     }
-    cout << fake[12215][1] << endl;
-    cout << endl;
-    cout << fake[12216][1] << endl;
-    cout << endl;
-    cout << fake[12217][1] << endl;
-    return 0;
+    cout << "Fake rows: " << rowIdx << endl; // 23481
+    return fakeN;
+}
+
+string (*readTrue())[4] { 
+    CSVReader reader("true.csv");
+    string (*trueN)[4] = new string[23600][4]; // head allcation of an array
+    int rowIdx = 0;
+
+    for (CSVRow &row : reader) {
+        int colIdx = 0;
+
+        for (CSVField &field : row) {
+            trueN[rowIdx][colIdx] = (field.get() == " " || (field.get()).empty()) ? "NULL" : field.get();
+            colIdx++;
+        }
+        rowIdx++;
+    }
+    cout << "True rows: " << rowIdx << endl; // 21417
+    return trueN;
+}
+
+int main() {
+    string (*fakeN)[4] = readFake();
+    string (*trueN)[4] = readTrue(); 
+
 }
