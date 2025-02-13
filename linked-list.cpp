530da6b881dcc4356fc89af058d83ac882902cf0
Aaron Ho <hoyanxun@gmail.com>
2025-02-13 15:18:34 +0800

    test for csv parser

---
+++ linked-list.cpp
@@ -0,0 +1,15 @@
+#include "csv.hpp"
+#include <iostream>
+
+using namespace csv;
+using namespace std;
+
+CSVReader reader("test.csv");
+
+int main() {
+  for (CSVRow &row : reader) {
+    for (CSVField &field : row) {
+      cout << field << endl;
+    }
+  }
+}
