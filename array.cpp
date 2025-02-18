bc3095da7f214731162c4cb4dd0ca794bbcda6bd
y <yyapwt@gmail.com>
2025-02-18 11:36:38 +0800

    update the array structure

--- array.cpp
+++ array.cpp
@@ -4,14 +4,104 @@
 #include "csv.hpp"
 #include "date.h"
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 using namespace csv;
 using namespace dateParsing;
+using namespace chrono;
 
+struct myArray {
+    private:
+        string dataCleaning(string col1) {
+            return (col1 == " " || (col1.empty())) ? "NULL" : col1;
+        }
+    
+        Date parseDate(string col3) {
+            // cout << "(Debug line 19)" << (Date(col3)).getDate() << endl;
+            return (Date(col3));
+        }
+
+    public: 
+        string col[3];
+        Date col3;
+
+        myArray() : col{ "", "NULL", "" }, col3(Date("")) {}
+
+        myArray (CSVRow &row) {
+            string data[4];
+            int rowIdx;
+
+            for (CSVField &field : row) {
+                data[rowIdx] = field.get();
+                rowIdx++;
+            }
+
+            this->col[0] = data[0];
+            this->col[1] = dataCleaning(data[1]);
+            this->col[2] = data[2];
+            this->col3 = parseDate(data[3]);
+        }
+
+        string operator[](int idx) {
+            if (idx >= 0 && idx < 3) {  
+                return col[idx];  
+            } else if (idx == 3) {  
+                return col3.getDate(); 
+            } else {
+                throw out_of_range("Index out of range");  // Throw an exception for invalid indices
+            }
+        }
+};
+
+myArray** readFile(string filename) {
+    CSVReader reader(filename);
+
+    myArray** file = new myArray*[23500];
+    int rowIdx = 0;
+
+    for (CSVRow &row : reader) {
+        file[rowIdx] = new myArray(row);
+        rowIdx++;
+    }
+    
+    (filename == "true.csv") ? cout << "True rows: " << rowIdx << endl : cout << "False rows: " << rowIdx << endl; // debug line
+    // true - 21417, false - 23481
+    return file;
+}
+
+void readArray(myArray** file) {
+    for (int i = 0; i < 23500; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << "data[" << i << "][" << j << "] = " << file[i]->operator[](j) << endl;
+        }
+    }
+}
+
+int main() {
+    
+    auto startF = high_resolution_clock::now();
+    myArray** fakeN = readFile("fake.csv");
+    auto stopF = high_resolution_clock::now();
+
+    cout << "FAke: " << (duration_cast<milliseconds>(stopF - startF)).count() << "ms" << endl;
+
+    auto startT = high_resolution_clock::now();
+    myArray** trueN = readFile("true.csv"); 
+    auto stopT = high_resolution_clock::now();
+
+    cout << "True: " << (duration_cast<milliseconds>(stopT - startT)).count() << "ms" << endl;
+
+
+
+    // myArray** testN = readFile("test.csv"); 
+    // readArray(testN);
+}
+
+/*
 string (*readFile(string filename))[4] { 
     CSVReader reader(filename);
-    string (*file)[4] = new string[23600][4]; // head allcation of an array
+    string (*file)[4] = new string[23500][4]; // head allcation of an array
     int rowIdx = 0;
 
     for (CSVRow &row : reader) {
@@ -30,25 +120,19 @@ string (*readFile(string filename))[4] {
         }
         rowIdx++;
     }
+
     (filename == "true.csv") ? cout << "True rows: " << rowIdx << endl : cout << "False rows: " << rowIdx << endl; // debug line
     // true - 21417, false - 23481
     return file;
 }
 
 void readArray(string (*news)[4]) {
-    for (int i = 0; i < 26000; i++) {
+    for (int i = 0; i < 23500; i++) {
         for (int j = 0; j < 4; j++) {
             cout << "data[" << i << "][" << j << "] = " << news[i][j] << std::endl;
         }
     }
 }
-
-int main() {
-    string (*fakeN)[4] = readFile("fake.csv");
-    string (*trueN)[4] = readFile("true.csv"); 
-    // string (*testN)[4] = readFile("test.csv"); 
-    // readArray(testN);
-}
-
+*/
 // sort
 // year-based sorting is fully demonstrated.
