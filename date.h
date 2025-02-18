00e1ec295c11845295853e19595756351657defc
y <yyapwt@gmail.com>
2025-02-18 18:26:46 +0800

    update date mapping

--- date.h
+++ date.h
@@ -12,9 +12,18 @@ namespace dateParsing {
     struct Date {
         private: 
             map<string, int> monthMap = {
-                {"January", 1},   {"February", 2}, {"March", 3},     {"April", 4},
-                {"May", 5},       {"June", 6},     {"July", 7},      {"August", 8},
-                {"September", 9}, {"October", 10}, {"November", 11}, {"December", 12}
+                {"January", 1}, {"Jan", 1},
+                {"February", 2}, {"Feb", 2},
+                {"March", 3}, {"Mar", 3},
+                {"April", 4}, {"Apr", 4},
+                {"May", 5},
+                {"June", 6}, {"Jun", 6},
+                {"July", 7}, {"Jul", 7},
+                {"August", 8}, {"Aug", 8},
+                {"September", 9}, {"Sep", 9},
+                {"October", 10}, {"Oct", 10},
+                {"November", 11}, {"Nov", 11},
+                {"December", 12}, {"Dec", 12}
             };
         public:
             int day, month, year;
