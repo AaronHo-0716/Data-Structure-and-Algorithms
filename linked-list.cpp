b777ac1e88c2729a266d2a9e3376f67c611aec82
Aaron Ho <hoyanxun@gmail.com>
2025-02-19 15:58:58 +0800

    Fixed the issue with traversing

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,8 +1,8 @@
 #include "csv.hpp"
 #include "date.h"
+#include <chrono>
 #include <iostream>
 #include <string>
-#include <chrono>
 
 using namespace csv;
 using namespace std;
@@ -24,22 +24,29 @@ struct Node {
     this->content = content;
     this->category = category;
     this->date = Date(date);
+
+    next = nullptr;
+    prev = nullptr;
+  }
+
+  string printNews() {
+    return title + "," + content + "," + category + "," + date.getDate();
   }
 };
 
 struct News {
   int counter;
-  Node* head;
-  Node* tail;
+  Node *head;
+  Node *tail;
 
   News() : counter(0), head(nullptr), tail(nullptr) {}
 
-  void printNewsCount() {
-    cout << counter << endl;
-  }
+  void printNewsCount() { cout << counter << endl; }
 
-  void insertAtBack(string title, string content, string category, string date) {
+  void insertAtBack(string title, string content, string category,
+                    string date) {
     Node *node = new Node(title, content, category, date);
+
     if (head == nullptr) {
       head = node;
       tail = node;
@@ -50,11 +57,23 @@ struct News {
     }
     counter++;
   }
+
+  void traverse() {
+    Node *current = head;
+    if (current == nullptr) {
+      cout << "News is empty" << endl;
+    } else {
+      while (current != nullptr) {
+        cout << current->printNews() << endl;
+        current = current->next;
+      }
+    }
+  }
 };
 
 int main() {
   News trueNews = News();
-  auto start = chrono::high_resolution_clock::now();
+  auto startOfReadingTrue = chrono::high_resolution_clock::now();
   for (CSVRow &row : reader) {
     string entries[4];
     int counter = 0;
@@ -64,8 +83,18 @@ int main() {
     }
     trueNews.insertAtBack(entries[0], entries[1], entries[2], entries[3]);
   }
-  auto stop = chrono::high_resolution_clock::now();
+  auto stopOfReadingTrue = chrono::high_resolution_clock::now();
   trueNews.printNewsCount();
-  auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
-  cout << "Time spent constructing the linked list: " << duration.count() << " miliseconds" << endl;
+  auto durationReadingTrue = chrono::duration_cast<chrono::milliseconds>(
+      stopOfReadingTrue - startOfReadingTrue);
+  cout << "Time spent constructing the linked list: "
+       << durationReadingTrue.count() << " miliseconds" << endl;
+
+  auto startOfPrintingTrue = chrono::high_resolution_clock::now();
+  trueNews.traverse();
+  auto stopOfPrintingTrue = chrono::high_resolution_clock::now();
+  auto durationPrintingTrue = chrono::duration_cast<chrono::milliseconds>(
+        stopOfPrintingTrue - startOfPrintingTrue);
+  cout << "Time spent printing all the true news: "
+       << durationReadingTrue.count() << " miliseconds" << endl;
 }
